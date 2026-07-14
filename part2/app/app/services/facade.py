bc5eacf9e58a793801ac9c717df888dd2ee16088
Ryma976 <ryma6110@gmail.com>
2026-07-14 18:22:06 +0300

    Implement user, amenity, and place management methods

--- part2/app/app/services/facade.py
+++ part2/app/app/services/facade.py
@@ -1,4 +1,7 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.amenity import Amenity
+from app.models.place import Place
 
 class HBnBFacade:
     def __init__(self):
@@ -6,3 +9,70 @@ class HBnBFacade:
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
+
+    # --- USER METHODS ---
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    # --- AMENITY METHODS ---
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    # --- PLACE METHODS ---
+    def create_place(self, place_data):
+        owner_id = place_data.get('owner_id')
+        owner = self.get_user(owner_id)
+        if not owner:
+            raise ValueError("Owner not found.")
+
+        amenity_ids = place_data.pop('amenity_ids', [])
+        place = Place(**place_data)
+        
+        for amenity_id in amenity_ids:
+            amenity = self.get_amenity(amenity_id)
+            if amenity:
+                place.add_amenity(amenity.id)
+
+        self.place_repo.add(place)
+        return place
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        place = self.get_place(place_id)
+        if not place:
+            raise ValueError("Place not found.")
+            
+        if 'owner_id' in place_data:
+            del place_data['owner_id']
+            
+        if 'amenity_ids' in place_data:
+            amenity_ids = place_data.pop('amenity_ids')
+            place.amenities = []
+            for amenity_id in amenity_ids:
+                amenity = self.get_amenity(amenity_id)
+                if amenity:
+                    place.add_amenity(amenity.id)
+
+        self.place_repo.update(place_id, place_data)
+        return place
