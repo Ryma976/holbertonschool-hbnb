377f24d2cbece17215a37faf278bbdadeb2d5d05
13222-crypto <13222@holbertonstudents.com>
2026-07-14 18:56:24 +0300

    Delete part2/amenity.py

--- part2/amenity.py
+++
@@ -1,9 +0,0 @@
-from app.models.base import BaseModel
-
-class Amenity(BaseModel):
-    def __init__(self, name, description=""):
-        super().__init__()
-        if not name or len(name.strip()) == 0:
-            raise ValueError("Amenity name cannot be empty.")
-        self.name = name.strip()
-        self.description = description.strip()
