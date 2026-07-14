5696b1023d0c43dbf5e2ad2bf3bd6b660448c2c2
13222-crypto <13222@holbertonstudents.com>
2026-07-14 19:35:41 +0300

    Update base.py

--- part2/app/models/base.py
+++ part2/app/models/base.py
@@ -7,9 +7,13 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-    def update(self, data=None):
-        if data:
-            for key, value in data.items():
-                if hasattr(self, key):
-                    setattr(self, key, value)
+    def save(self):
+        """تحديث وقت التعديل عند حفظ البيانات"""
         self.updated_at = datetime.now()
+
+    def update(self, data):
+        """تحديث الخصائص ديناميكياً"""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()
