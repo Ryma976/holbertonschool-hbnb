7248b4919e500d8b4892b54d483b24cc44d52875
Ryma976 <ryma6110@gmail.com>
2026-07-15 16:18:54 +0300

    Refactor create_app to update API description
    Updated the API description in the create_app function and removed the reviews namespace registration.

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from flask_restx import Api
 from app.services.facade import HBnBFacade
@@ -6,24 +7,15 @@ facade = HBnBFacade()
 
 def create_app():
     app = Flask(__name__)
-    
-    api = Api(
-        app, 
-        version='1.0', 
-        title='HBnB API', 
-        description='HBnB Application API Documentation',
-        doc='/api/v1/'
-    )
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Evolution Application API')
 
-    # استيراد مساحات الأسماء لجميع الميزات وتسجيلها
+    # Register all namespaces
     from app.api.v1.users import api as users_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.places import api as places_ns
-    from app.api.v1.reviews import api as reviews_ns
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
     return app
