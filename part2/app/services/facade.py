a3ec1429d8ac54f41da823c9e021661b6bca5760
Ryma976 <ryma6110@gmail.com>
2026-07-15 15:57:38 +0300

    Refactor HBnBFacade to include amenities and places

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,22 +1,18 @@
-from app.models.user import User
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.amenity import Amenity
+from app.models.place import Place
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
 
-    # --- عمليات المستخدمين (User Operations) ---
+    # --- USER METHODS ---
     def create_user(self, user_data):
-        # التحقق من عدم تكرار البريد الإلكتروني
-        for u in self.user_repo.get_all():
-            if u.email == user_data['email']:
-                raise ValueError("Email already exists.")
-        
-        user = User(
-            first_name=user_data['first_name'],
-            last_name=user_data['last_name'],
-            email=user_data['email']
-        )
+        user = User(**user_data)
         self.user_repo.add(user)
         return user
 
@@ -24,13 +20,76 @@ class HBnBFacade:
         return self.user_repo.get(user_id)
 
     def get_user_by_email(self, email):
-        for u in self.user_repo.get_all():
-            if u.email == email:
-                return u
-        return None
+        return self.user_repo.get_by_attribute('email', email)
 
     def get_all_users(self):
         return self.user_repo.get_all()
 
     def update_user(self, user_id, user_data):
-        return self.user_repo.update(user_id, user_data)
+        user = self.get_user(user_id)
+        if not user:
+            raise ValueError("User not found.")
+        self.user_repo.update(user_id, user_data)
+        return user
+
+    # --- AMENITY METHODS ---
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.get_amenity(amenity_id)
+        if not amenity:
+            raise ValueError("Amenity not found.")
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return amenity
+
+    # --- PLACE METHODS ---
+    def create_place(self, place_data):
+        owner_id = place_data.get('owner_id')
+        owner = self.get_user(owner_id)
+        if not owner:
+            raise ValueError("Owner not found.")
+
+        amenity_ids = place_data.pop('amenity_ids', [])
+        place = Place(**place_data)
+        
+        for amenity_id in amenity_ids:
+            amenity = self.get_amenity(amenity_id)
+            if amenity:
+                place.add_amenity(amenity.id)
+
+        self.place_repo.add(place)
+        return place
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        place = self.get_place(place_id)
+        if not place:
+            raise ValueError("Place not found.")
+            
+        if 'owner_id' in place_data:
+            del place_data['owner_id']
+            
+        if 'amenity_ids' in place_data:
+            amenity_ids = place_data.pop('amenity_ids')
+            place.amenities = []
+            for amenity_id in amenity_ids:
+                amenity = self.get_amenity(amenity_id)
+                if amenity:
+                    place.add_amenity(amenity.id)
+
+        self.place_repo.update(place_id, place_data)
+        return place
