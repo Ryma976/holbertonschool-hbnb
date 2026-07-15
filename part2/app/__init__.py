2081d62105d92e09730d472d08b012a3cf318ed7
13222-crypto <13222@holbertonstudents.com>
2026-07-15 16:04:37 +0300

    Update __init__.py

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_restx import Api
-
-# نقوم بتعريف الواجهة بشكل مرن لتجنب مشاكل الاستيراد الدائري
 from app.services.facade import HBnBFacade
+
 facade = HBnBFacade()
 
 def create_app():
@@ -16,8 +15,15 @@ def create_app():
         doc='/api/v1/'
     )
 
-    # تسجيل الـ Namespace الخاص بالمستخدمين
+    # استيراد مساحات الأسماء لجميع الميزات وتسجيلها
     from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as reviews_ns
+
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
     return app
