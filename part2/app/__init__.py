4c5104c0b3cb8fd4745abfe8f299d12400fb6851
13222-crypto <13222@holbertonstudents.com>
2026-07-16 14:25:27 +0300

    Update __init__.py

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,20 +1,17 @@
 from flask import Flask
 from flask_restx import Api
-from app.services.facade import HBnBFacade
-
-facade = HBnBFacade()
+from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns # تأكدي من وجود هذا السطر
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Evolution Application API')
-
-    # Register all namespaces
-    from app.api.v1.users import api as users_ns
-    from app.api.v1.amenities import api as amenities_ns
-    from app.api.v1.places import api as places_ns
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews') # وتأكدي من وجود هذا السطر
 
     return app
