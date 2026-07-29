8cbb42152653c48fdcd8b78fc9f5641c001259a2
reem <ryma6110@gmail.com>
2026-07-29 19:00:28 +0200

    Fix Amenity model and app initialization for database setup

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,45 +1,34 @@
 from flask import Flask
-from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from flask_sqlalchemy import SQLAlchemy
-from config import config
+from flask_restx import Api
 
+db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
-db = SQLAlchemy()
 
-def create_app(config_class="default"):
-    app = Flask(__name__)
-    
-    if isinstance(config_class, str):
-        app.config.from_object(config.get(config_class, config['default']))
-    else:
-        app.config.from_object(config_class)
+def create_app(config_class="config.DevelopmentConfig"):
+    flask_app = Flask(__name__)
+    flask_app.config.from_object(config_class)
 
-    bcrypt.init_app(app)
-    jwt.init_app(app)
-    db.init_app(app)
+    db.init_app(flask_app)
+    bcrypt.init_app(flask_app)
+    jwt.init_app(flask_app)
 
-    with app.app_context():
-        import app.models.user
-        import app.models.place
-        import app.models.review
-        import app.models.amenity
-        db.create_all()
+    api = Api(flask_app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/endpoints/')
 
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
-
+    # Register Namespaces/Blueprints
     from app.api.v1.users import api as users_ns
-    from app.api.v1.auth import api as auth_ns
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
     from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.auth import api as auth_ns
 
     api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
-    return app
+    return flask_app
