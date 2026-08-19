77092eeed0a3f31f9e825f5d540d178c8c0db608
root <ryma6110@gmail.com>
2026-08-19 13:35:30 +0000

    Fix CORS, update run.py config, and implement frontend login/places tasks

--- part3/run.py
+++ part3/run.py
@@ -1,6 +1,8 @@
 from app import create_app
+from config import DevelopmentConfig
 
-app = create_app('development')
+
+app = create_app(DevelopmentConfig)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
